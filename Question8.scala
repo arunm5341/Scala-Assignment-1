e8063421d0296c084629e9b757aed106e75bdddb
arunm5341 <84780164+arunm5341@users.noreply.github.com>
2021-06-07 18:06:34 +0530

    Happy Character v/s sad Character

---
+++ Question8.scala
@@ -0,0 +1,9 @@
+object Question8 {
+  def main(args: Array[String]): Unit = {
+    var  str: String = "1arun1"
+    var i: Char = str.charAt(0)
+    var z: Char = str.charAt(5)
+    if ((i==Int) || (z==Int)) println("Happy Character")
+    else println("Sad Character")
+  }
+}
